cc687d8ae5bc54f2a4d1956aa44104f77186b20d
serg-ib <92174801+serg-ib@users.noreply.github.com>
2021-10-11 11:05:41 +0500

    Update scripts1.js

--- js/scripts1.js
+++ js/scripts1.js
@@ -3,7 +3,7 @@ const totalPriceTag = document.querySelector('.price-total');
 let cost=800;
 let totalPrice=0;
 schemeSvg.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('booked') && !event.target.classList.contains('light')){
+  if (!event.target.classList.contains('booked') OR !event.target.classList.contains('light')){
     event.target.classList.toggle('active');
     let totalSeats = schemeSvg.querySelectorAll('.active').length;
     totalPrice = totalSeats *cost;
